5dfeb1e663742250592a9828f19e40c0d39899b1
hikettei <hikettei@gmail.com>
2023-05-18 15:16:13 +0900

    text generator

--- main.py
+++ main.py
@@ -20,7 +20,17 @@ from SLT.model import (
     SaltGPT
     )
 
+from tqdm import tqdm
+
 #from torchviz import make_dot
+
+def padding(x, y):
+    cond = len(x) >= len(y)
+    longer_one = y if cond else x
+    diff = abs(len(x) - len(y))
+    for i in range(diff):
+        longer_one.append(0)
+    return (x, longer_one) if cond else (longer_one, y)
 
 config = SaltConfig(opt_forward=False,
                     opt_backward=False,
@@ -33,7 +43,7 @@ config.use_embedding = weights
 config.vocab_size = weights.size()[0]
 config.embedding_dim = weights.size()[1]
 
-source = """
+source = """[BOS]
 Inspired by the relationship between the ODE and neural networks [25, 8], we first show that the
 Transformer layers can be naturally interpreted as a numerical ODE solver for a first-order convectiondiffusion equation in MPDS. To be more specific, the self-attention sub-layer, which transforms
 the semantics at one position by attending over all other positions, corresponds to the diffusion
@@ -48,7 +58,7 @@ collection of particles, and the latent representations abstracted in stacked Tr
 viewed as the location of particles moving in a high-dimensional space at different time points
 """
 
-target = """
+target = """[BOS]
 Such an interpretation not only provides a new perspective on the Transformer but also inspires us
 to design new structures by leveraging the rich literature of numerical analysis. The Lie-Trotter
 splitting scheme is simple but not accurate and often leads to high approximation error [17]. The
@@ -74,24 +84,61 @@ optimizer = optim.Adam(model.parameters(), lr=1e-4)
 criterion = nn.CrossEntropyLoss()
 
 x_sentence = bpe_tokenizer.encode(source)
-y_sentence = x_sentence.copy()#bpe_tokenizer.encode(target)
+y_sentence = bpe_tokenizer.encode(target)
 
-print(model)
+x_sentence, y_sentence = padding(x_sentence, y_sentence)
 
 x = torch.tensor(x_sentence).unsqueeze(0)
 y = torch.tensor(y_sentence).unsqueeze(0)
 
+print(x.size())
+print(y.size())
+
 def train(config, model, optimizer, x, y):
+    model.train()
     optimizer.zero_grad()
     y_out = step_model(config, model, x, y)
     loss = 0.0
-
     for n in range(y.size(1)):
         loss += criterion(y_out[:, n, :], y[:, n])
     loss.backward()
+    print(f"loss: {loss / y.size(1)}")
     optimizer.step()
+    generate_sentence(config, model, "[BOS] Inspired by the relationship.")
 
-train(config, model, optimizer, x, y)
+def generate_sentence(config, model, source, input_more="", sentence_len=50):
+    x_first = source
+    y_first = "[BOS]" + input_more
+    
+    x_first = bpe_tokenizer.encode(x_first)
+    y_first = bpe_tokenizer.encode(y_first)
+
+    x, y = padding(x_first, y_first)
+
+    for _ in range(sentence_len):
+        x.append(0)
+        y.append(0)
+    
+    x = torch.tensor(x).unsqueeze(0)
+    y = torch.tensor(y).unsqueeze(0)
+
+    model.eval()
+
+    with torch.no_grad():
+        y_out = step_model(config, model, x, y)
+        y_decode = torch.argmax(y_out, dim=-1)
+        print(bpe_tokenizer.decode(y_decode[0].tolist()))
+
+generate_sentence(config, model, "[BOS] Inspired by the relationship")
+    
+
+for i in tqdm(range(10)):
+    train(config, model, optimizer, x, y)
+
+while True:
+    source = input("> Input Something...")
+    generate_sentence(config, model, source)
+    
 
 #img = make_dot(y, params=dict(model.named_parameters()))
 #img.render("model_structure_v1", format="png")
